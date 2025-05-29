0545c7df8d255cc488772924899e310d6f6cb3fc
Mark <Mark@LATITUDE>
2025-05-29 10:43:40 -0700

    Added test for NTS library and updated readme

---
+++ FileBaseContext.Tests/Data/Entities/Place.cs
@@ -0,0 +1,10 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace FileBaseContext.Tests.Data.Entities;
+
+public class Place
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public Point Location { get; set; }
+}
